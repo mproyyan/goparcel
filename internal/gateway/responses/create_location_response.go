e314d10b5e0b60c99dbb1a050630d8e9281f06dc
mproyyan <mproyyan@gmail.com>
2025-02-17 09:48:13 +0700

    create api gateway to connect to GetLocation rpc

--- internal/gateway/responses/create_location_response.go
+++ internal/gateway/responses/create_location_response.go
@@ -1,1 +1,19 @@
 package responses
+
+type Address struct {
+	Province      string  `json:"province"`
+	City          string  `json:"city"`
+	District      string  `json:"district"`
+	Subdistrict   string  `json:"subdistrict,omitempty"`
+	Latitude      float64 `json:"latitude,omitempty"`
+	Longitude     float64 `json:"longitude,omitempty"`
+	StreetAddress string  `json:"street_address,omitempty"`
+	ZipCode       string  `json:"zip_code"`
+}
+
+type LocationResponse struct {
+	Name        string  `json:"name"`
+	Type        string  `json:"type"`
+	WarehouseID string  `json:"warehouse_id,omitempty"`
+	Address     Address `json:"address"`
+}
--- internal/gateway/services/locationService.go
+++ internal/gateway/services/locationService.go
@@ -50,8 +50,43 @@ func (l LocationService) createLocation(c *fiber.Ctx) error {
 	})
 }
 
+func (l LocationService) findLocations(c *fiber.Ctx) error {
+	// Get id from url parameter
+	locationID := c.Params("id")
+
+	// Call GetLocation RPC
+	location, err := l.client.GetLocation(c.Context(), &locations.GetLocationRequest{
+		LocationID: locationID,
+	})
+
+	if err != nil {
+		code, errResponse := responses.NewErrorResponse(err)
+		return c.Status(code).JSON(errResponse)
+	}
+
+	// Build response
+	locationResponse := responses.LocationResponse{
+		Name:        location.Name,
+		Type:        location.Type,
+		WarehouseID: location.WarehouseId,
+		Address: responses.Address{
+			Province:      location.Address.Province,
+			City:          location.Address.City,
+			District:      location.Address.District,
+			Subdistrict:   location.Address.Subdistrict,
+			Latitude:      location.Address.Latitude,
+			Longitude:     location.Address.Longitude,
+			StreetAddress: location.Address.StreetAddress,
+			ZipCode:       location.Address.ZipCode,
+		},
+	}
+
+	return c.Status(fiber.StatusOK).JSON(locationResponse)
+}
+
 func (l LocationService) Bootstrap() {
 	location := l.router.Group("/locations")
 
 	location.Post("/", l.createLocation)
+	location.Get("/:id", l.findLocations)
 }
